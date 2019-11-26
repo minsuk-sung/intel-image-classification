c360330e1eac08e87e84a70e6631410a2167e149
Minsuk Sung <mssung94@gmail.com>
2019-11-27 02:15:46 +0900

    Create download-data.sh

---
+++ download-data.sh
@@ -0,0 +1,13 @@
+cd data
+
+echo 'Downloading Fashion MNIST Files'
+wget https://drive.google.com/open?id=1H-hCU9xULhORBbSlnyAXfzJPR-6myFai
+
+echo 'Downloading Kaggle Cat & Dog Files'
+wget https://drive.google.com/open?id=1udAgSe0hqUzkqvl_PoHIYvCD-V_z-v0B
+
+echo 'Downloading Kaggle Intel Scene Image Files'
+wget https://drive.google.com/open?id=1ec4Urjxvif0fenMyl6Jl1A1SW104yiDW
+
+echo 'Downloading VOC2012 Files'
+wget https://drive.google.com/open?id=1k5U4ekqwYT7993Cl4gFZHxBTFM3h6RLF
